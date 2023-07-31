981dddcb937bba31b8502ad02c87674a20aaba58
El0y C0sm0 <eloymaciel06@gmail.com>
2023-07-31 16:44:55 -0300

    template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+// Os dois defines abaixo servem para deixar entradas e saidas mais rápido
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define pb push_back
+#define I_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define dbg(a) (cout << #a << " = " << a << '\n')
+#define loop(i,m) for(int i = 0; i < m; ++i)
+#define min(a,b) ((a<b)?a:b)
+#define max(a,b) ((a>b)?a:b)
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef long long ll;
+typedef vector<int> vtr;
+
+int main() {_
+
+    return 0;
+}
