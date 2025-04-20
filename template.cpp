27868efbe19cdcd6a14093cef92eab34bdac56be
Eloy Maciel <105126138+El0y-C0SM0@users.noreply.github.com>
2025-04-19 21:20:37 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 
-// Os dois defines abaixo servem para deixar entradas e saidas mais rápido
-#define endl '\n'
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define pb push_back
-#define I_INF 0x3f3f3f3f
-#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define endl '\n'
 #define dbg(a) (cout << #a << " = " << a << '\n')
-#define loop(i,m) for(int i = 0; i < m; ++i)
-#define min(a,b) ((a<b)?a:b)
-#define max(a,b) ((a>b)?a:b)
+#define loop(i,a,m) for(int i = a; i < m; ++i)
+#define pb push_back
+#define f first
+#define s second
 
 using namespace std;
 
-typedef unsigned int uint;
+typedef uint32_t uint;
 typedef long long ll;
+typedef long double ld;
 typedef vector<int> vtr;
+typedef pair<int,int> par;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 int main() {_
 
